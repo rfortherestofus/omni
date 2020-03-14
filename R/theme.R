6ecbda0078ce6dd947a15d40827345bcc81d0347
David Keyes <dgkeyes@gmail.com>
2020-03-13 17:20:22 -0700

    Add theme_omni

---
+++ R/theme.R
@@ -0,0 +1,29 @@
+#' OMNI Institute ggplot2 theme
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_omni <- function(grid_lines = TRUE,
+                       base_theme = ggplot2::theme_minimal(base_family = "Lato")) {
+
+
+  omni_theme <- base_theme +
+    ggplot2::theme(panel.grid.minor = element_blank(),
+                   axis.ticks = element_blank(),
+                   axis.title.x = element_text(margin = ggplot2::margin(15, 0, 0, 0)),
+                   axis.title.y = element_text(margin = ggplot2::margin(0, 15, 0, 0)),
+                   plot.title = element_text(margin = ggplot2::margin(0, 0, 15, 0),
+                                             face = "bold"),)
+
+  if (grid_lines == FALSE) {
+
+    omni_theme <- omni_theme +
+      ggplot2::theme(panel.grid.major = element_blank())
+
+  }
+
+  omni_theme
+
+}
+
