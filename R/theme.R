5a4cfe64a488370d84be74c74fbd5787e86d9659
David Keyes <dgkeyes@gmail.com>
2020-11-09 14:26:04 -0800

    updates

--- R/theme.R
+++ R/theme.R
@@ -2,29 +2,19 @@
 #'
 #' @param grid_lines Boolean to indicate whether to have grid_lines (TRUE by default)
 #' @param show_legend Whether or not to show the legend (TRUE by default)
+#' @param base_theme The base theme used (theme_minimal() with the Lato font)
 #'
 #' @return
 #' @export
 #'
 #' @examples
 theme_omni <- function(show_grid_lines = TRUE,
-                       show_legend = TRUE) {
+                       show_legend = TRUE,
+                       base_theme = ggplot2::theme_minimal(base_family = "Lato")) {
 
-  # Determine user OS
-  # https://stackoverflow.com/questions/4463087/detecting-operating-system-in-r-e-g-for-adaptive-rprofile-files
-  user_os <- Sys.info()[['sysname']]
+  import_lato()
 
-  if (user_os == "Windows") {
-    grDevices::windowsFonts(`Lato` = grDevices::windowsFont("Lato"))
-    grDevices::windowsFonts(`Lato Light` = grDevices::windowsFont("Lato Light"))
-    grDevices::windowsFonts(`Lato Black` = grDevices::windowsFont("Lato Black"))
-  } else {
-    suppressWarnings(suppressMessages(extrafont::font_import(pattern = "Lato", prompt=FALSE)))
-  }
-
-  usethis::ui_done("Lato font has been imported and can be used")
-
-  omni_theme <- ggplot2::theme_minimal(base_family = "Lato") +
+  omni_theme <- base_theme +
     ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank(),
                    axis.title.x = ggplot2::element_text(margin = ggplot2::margin(15, 0, 0, 0)),
