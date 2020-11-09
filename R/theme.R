bd2761a2628da550f4d52b93c0dea4379469c9da
David Keyes <dgkeyes@gmail.com>
2020-11-09 14:27:19 -0800

    updates

--- R/theme.R
+++ R/theme.R
@@ -2,19 +2,17 @@
 #'
 #' @param grid_lines Boolean to indicate whether to have grid_lines (TRUE by default)
 #' @param show_legend Whether or not to show the legend (TRUE by default)
-#' @param base_theme The base theme used (theme_minimal() with the Lato font)
 #'
 #' @return
 #' @export
 #'
 #' @examples
 theme_omni <- function(show_grid_lines = TRUE,
-                       show_legend = TRUE,
-                       base_theme = ggplot2::theme_minimal(base_family = "Lato")) {
+                       show_legend = TRUE) {
 
   import_lato()
 
-  omni_theme <- base_theme +
+  omni_theme <- ggplot2::theme_minimal(base_family = "Lato") +
     ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank(),
                    axis.title.x = ggplot2::element_text(margin = ggplot2::margin(15, 0, 0, 0)),
