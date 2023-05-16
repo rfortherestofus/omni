4e1ec089b49a87570a40ecd2dcc70d51da6078a3
Thomas Vroylandt <tvroylandt@gmail.com>
2023-05-16 11:56:21 +0200

    Adding wrappers to list and fetch #24

--- R/qualtromni.R
+++ R/qualtromni.R
@@ -16,10 +16,12 @@ qualtromni_register <- function(people_name, api_key, base_url) {
   if (!file.exists(renv)) {
     file.create(renv)
   }
-  keyconcat <- paste0("QUALTRICS_API_KEY_", people_name, " = '", api_key,
-                      "'")
-  urlconcat <- paste0("QUALTRICS_BASE_URL_", people_name, " = '", base_url,
-                      "'")
+  keyconcat <-
+    paste0("QUALTRICS_API_KEY_", people_name, " = '", api_key,
+           "'")
+  urlconcat <-
+    paste0("QUALTRICS_BASE_URL_", people_name, " = '", base_url,
+           "'")
   write(keyconcat, renv, sep = "\n", append = TRUE)
   write(urlconcat, renv, sep = "\n", append = TRUE)
 
@@ -33,21 +35,42 @@ qualtromni_register <- function(people_name, api_key, base_url) {
 qualtromni_connect <- function(people_name) {
   readRenviron("~/.Renviron")
 
-  suppressMessages(qualtrics_api_credentials(api_key = Sys.getenv(
-    paste0("QUALTRICS_API_KEY_", people_name)
-  ),
-  base_url = Sys.getenv(
-    paste0("QUALTRICS_BASE_URL_", people_name)
-  )))
+  suppressMessages(
+    qualtRics::qualtrics_api_credentials(api_key = Sys.getenv(
+      paste0("QUALTRICS_API_KEY_", people_name)
+    ),
+    base_url = Sys.getenv(
+      paste0("QUALTRICS_BASE_URL_", people_name)
+    ))
+  )
 }
 
-#' Title
+#' Get surveys
 #'
-#' @return
+#' @return A tibble with all surveys
 #' @export
 #'
-#' @examples
 qualtromni_list_survey <- function() {
-
+  qualtRics::all_surveys()
 }
 
+#' Get survey by name of ID
+#'
+#' @param survey_id_name Survey name or ID, whatever
+#'
+#' @return A survey
+#' @export
+#'
+qualtromni_get_survey <- function(survey_id_name) {
+  if (substr(survey_id_name, 1, 3) == "SV_") {
+    # by ID
+    qualtRics::fetch_survey(survey_id_name)
+  } else{
+    # by name
+    survey_id <- all_surveys() |>
+      dplyr::filter(name == survey_id_name) |>
+      dplyr::pull(id)
+
+    qualtRics::fetch_survey(survey_id)
+  }
+}
