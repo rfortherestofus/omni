3301c6e47ab31053ed6f6d247665a6ce41c14915
Thomas Vroylandt <tvroylandt@gmail.com>
2023-08-31 21:53:36 +0200

    Adding gt table function

--- R/table.R
+++ R/table.R
@@ -3,6 +3,7 @@
 #' This function creates a table in OMNI's style.
 #'
 #' @param df The data frame to be put into the table
+#' @param option flextable (by default) or gt
 #'
 #' @return A table themed
 #' @export
@@ -14,30 +15,71 @@
 #' @importFrom knitr include_graphics
 #'
 
-omni_table <- function(df) {
-  # table theme
-  table <- df |>
-    flextable() |>
-    theme_zebra(even_body = "#9DAECE",
-                odd_body = "#CED6E6")  |>
-    fontsize(part = "all", size = 11) |>
-    font(part = "all", fontname = "Calibri") |>
-    bold(part = "header", bold = TRUE) |>
-    bold(part = "body", j = 1, bold = TRUE) |>
-    align(part = "all", align = "center") |>
-    bg(part = "header", bg = omni_colors("Dark Blue")) |>
-    color(part = "header", color = "white") |>
-    color(part = "body", color = "#333333") |>
-    bg(part = "body",
-       j = 1,
-       bg = omni_colors("Dark Blue")) |>
-    color(part = "body",
-          j = 1,
-          color = "white") |>
-    height_all(height = 0.4) |>
-    border_inner(part = "body", border = fp_border(color = "white")) |>
-    border(part = "header", border.bottom = fp_border(color = "white"))
-
+omni_table <- function(df, option = "flextable") {
+  if (option == "flextable")
+  {
+    # table theme with flextable
+    table <- df |>
+      flextable() |>
+      theme_zebra(even_body = "#9DAECE",
+                  odd_body = "#CED6E6")  |>
+      fontsize(part = "all", size = 11) |>
+      font(part = "all", fontname = "Calibri") |>
+      bold(part = "header", bold = TRUE) |>
+      bold(part = "body", j = 1, bold = TRUE) |>
+      align(part = "all", align = "center") |>
+      bg(part = "header", bg = omni_colors("Dark Blue")) |>
+      color(part = "header", color = "white") |>
+      color(part = "body", color = "#333333") |>
+      bg(part = "body",
+         j = 1,
+         bg = omni_colors("Dark Blue")) |>
+      color(part = "body",
+            j = 1,
+            color = "white") |>
+      height_all(height = 0.4) |>
+      border_inner(part = "body", border = fp_border(color = "white")) |>
+      border(part = "header",
+             border.bottom = fp_border(color = "white"))
+  } else if (option == "gt") {
+    # table theme with gt
+    table <- df |>
+      gt() |>
+      tab_style(style = list(cell_text(
+        font = "Calibri",
+        size = 11,
+        color = "#333333"
+      )),
+      locations = list(cells_body())) |>
+      tab_style(
+        style = list(
+          cell_text(
+            font = "Calibri",
+            size = 11,
+            weight = "bold",
+            color = "white"
+          ),
+          cell_fill(color = omni_colors("Dark Blue"))
+        ),
+        locations = list(cells_column_labels())
+      ) |>
+      tab_style(style = list(
+        cell_text(weight = "bold",
+                  color = "white"),
+        cell_fill(color = omni_colors("Dark Blue"))
+      ),
+      locations = cells_body(columns = 1)) |>
+      tab_style(style = cell_borders(color = "white"),
+                locations = cells_body()) |>
+      tab_style(style = cell_borders("b", color = "white"),
+                locations = cells_column_labels()) |>
+      cols_align(align = "center") |>
+      tab_options(
+        table.background.color = "#9DAECE",
+        row.striping.background_color  = "#CED6E6"
+      ) |>
+      opt_row_striping()
+  }
   # return table
   table
 }
