89f1adaf89b5bec0aa4c692d128d7b2c5b611a97
Thomas Vroylandt <tvroylandt@gmail.com>
2023-12-11 21:23:58 +0100

    Init defauflt #49

---
+++ R/ggplot_defaults.R
@@ -0,0 +1,111 @@
+#' Update defaults to OMNI's theme
+#'
+#'
+#' @param base_family The base font family for the theme.
+#' @param base_color Base color
+#'
+#' @import ggplot2
+#' @import ggrepel
+#' @md
+#' @export
+set_omni_defaults <- function(base_family = "Calibri",
+                              base_color = "#314160") {
+  # set default theme -----------------------------------------------------
+
+  ggplot2::theme_set(omni_theme(base_family = base_family))
+
+  # add base_family font to text and label geoms ---------------------------
+
+  ggplot2::update_geom_defaults("text", list(family = base_family))
+  ggplot2::update_geom_defaults("label", list(family = base_family))
+  ggplot2::update_geom_defaults("text_repel", list(family = base_family))
+  ggplot2::update_geom_defaults("label_repel", list(family = base_family))
+
+  # set default color scales for continuous variables -----------------------
+
+  options(ggplot2.continuous.colour = "gradient",
+          ggplot2.continuous.fill = "gradient")
+
+  # set default colors for monochromatic geoms ------------------------------
+
+  ggplot2::update_geom_defaults("bar", list(fill = base_color))
+  ggplot2::update_geom_defaults("col", list(fill = base_color))
+  ggplot2::update_geom_defaults("point", list(colour = base_color))
+  ggplot2::update_geom_defaults("line", list(colour = base_color))
+  ggplot2::update_geom_defaults("step", list(colour = base_color))
+  ggplot2::update_geom_defaults("path", list(colour = base_color))
+  ggplot2::update_geom_defaults("boxplot", list(fill = base_color))
+  ggplot2::update_geom_defaults("density", list(fill = base_color))
+  ggplot2::update_geom_defaults("violin", list(fill = base_color))
+
+  # set default colors for monochromatic stats ------------------------------
+
+  ggplot2::update_stat_defaults("count", list(fill = base_color))
+  ggplot2::update_stat_defaults("boxplot", list(fill = base_color))
+  ggplot2::update_stat_defaults("density", list(fill = base_color))
+  ggplot2::update_stat_defaults("ydensity", list(fill = base_color))
+
+  # width and sizes ---------------------------------------------------------
+
+  # this aren't aes
+  # --> will need to solve something about importing ggplot2 order
+  formals(geom_bar)$width <- 0.7
+  formals(geom_col)$width <- 0.7
+
+  # this are
+  ggplot2::update_geom_defaults("line", list(linewidth = 1))
+  ggplot2::update_geom_defaults("step", list(linewidth = 1))
+  ggplot2::update_geom_defaults("path", list(linewidth = 1))
+  ggplot2::update_geom_defaults("point", list(size = 3))
+  ggplot2::update_geom_defaults("text", list(size = 1 / 0.352777778))
+
+}
+
+#' Resets to default setting
+#'
+#' @import ggplot2
+#' @import ggrepel
+#'
+#' @export
+#'
+ggplot_defaults <- function() {
+  ggplot2::theme_set(ggplot2::theme_grey())
+
+  # add Helvetica font to text and label geoms ---------------------------
+
+  ggplot2::update_geom_defaults("text", list(family = "Helvetica"))
+  ggplot2::update_geom_defaults("label", list(family = "Helvetica"))
+  ggplot2::update_geom_defaults("text_repel", list(family = "Helvetica"))
+  ggplot2::update_geom_defaults("label_repel", list(family = "Helvetica"))
+
+  # set default colours for monochromatic geoms -----------------------------
+
+  ggplot2::update_geom_defaults("bar", list(fill = "#595959"))
+  ggplot2::update_geom_defaults("col", list(fill = "#595959"))
+  ggplot2::update_geom_defaults("point", list(colour = "black"))
+  ggplot2::update_geom_defaults("line", list(colour = "#595959"))
+  ggplot2::update_geom_defaults("boxplot", list(fill = "#595959"))
+  ggplot2::update_geom_defaults("density", list(fill = "#595959"))
+  ggplot2::update_geom_defaults("violin", list(fill = "#595959"))
+
+  # set default colours for monochromatic stats -----------------------------
+
+  ggplot2::update_stat_defaults("count", list(fill = "#595959"))
+  ggplot2::update_stat_defaults("boxplot", list(fill = "#595959"))
+  ggplot2::update_stat_defaults("density", list(fill = "#595959"))
+  ggplot2::update_stat_defaults("ydensity", list(fill = "#595959"))
+
+  # width and sizes ---------------------------------------------------------
+
+  # this aren't aes
+  formals(geom_bar)$width <- NULL
+  formals(geom_col)$width <- NULL
+
+  # this are
+  ggplot2::update_geom_defaults("line", list(linewidth = .5))
+  ggplot2::update_geom_defaults("step", list(linewidth = .5))
+  ggplot2::update_geom_defaults("path", list(linewidth = .5))
+  ggplot2::update_geom_defaults("point", list(size = 1.5))
+  ggplot2::update_geom_defaults("text", list(size = 3.88))
+
+}
