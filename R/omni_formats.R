8d3d1799bdb0bfd2ddcd2b209a847eaaf583509b
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-05-09 13:55:00 +0200

    fix page breaks

--- R/omni_formats.R
+++ R/omni_formats.R
@@ -6,23 +6,10 @@
 #' @export
 #'
 omni_pdf_report <- function(...) {
-  # style
-  css_style <- system.file(
-    "rmarkdown",
-    "templates",
-    "report-pdf",
-    "skeleton",
-    "omni_pdf_report.css",
-    package = "omni",
-    mustWork = TRUE
-  )
-
-  # template
   pagedown::html_paged(
     self_contained = TRUE,
     toc = TRUE,
     fig_caption = TRUE,
-    #css = css_style,
     ...
   )
 }
