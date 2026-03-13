36e72e5b1a5fb28640b50c82efa06e28cefe3520
Albert Rapp <AlbertRapp@gmx.de>
2026-03-13 11:59:55 +0100

    Create brand template and corresponding printing function

---
+++ R/get_brand_template.R
@@ -0,0 +1,52 @@
+brand_template <- function() {
+  # Begin default branding (leave line for get_brand_template() function)
+  Brand(
+    meta = BrandMeta(name = "brand.yml"),
+    color = BrandColor(
+      foreground = "#081c39",
+      background = "#ffffff",
+      primary = "#081c39",
+      secondary = "#677384",
+      my_custom_col1 = '#ff5e34',
+      my_custom_col2 = '#f7b925'
+    ),
+    typography = BrandTypography(
+      fonts = list(
+        Font(family = 'Open Sans', source = 'google'),
+        Font(family = 'Inter Tight', source = 'google'),
+        Font(
+          family = 'Helvetica Neue',
+          source = 'system',
+          dir_source = 'inst/fonts/helvetica_neue'
+        )
+      ),
+      base = FontSpec(
+        family = 'Helvetica Neue',
+        'line-height' = 1.6
+      ),
+      headings = FontSpec(
+        family = "Inter Tight",
+        weight = "bold"
+      ),
+      link = FontSpec(color = "#5776b2")
+    )
+  )
+  # End default branding (leave line for get_brand_template() function)
+}
+
+
+print_brand_template <- function() {
+  path_default_brand_function <- system.file(
+    "R/get_brand_template.R",
+    package = "omni",
+    mustWork = TRUE
+  )
+  lines_default_brand <- readLines(path_default_brand_function)
+  idx_lines_from_to <- stringr::str_which(
+    lines_default_brand,
+    "(Begin|End) default branding"
+  ) +
+    c(1, -1)
+  lines_default_brand[idx_lines_from_to[1]:idx_lines_from_to[2]] |>
+    cat(sep = '\n')
+}
