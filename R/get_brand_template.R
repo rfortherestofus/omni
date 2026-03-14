5bf9a7feb52046f10450acc0f6b43cf5dbff09bb
Albert Rapp <AlbertRapp@gmx.de>
2026-03-14 16:24:26 +0100

    Document and install package

--- R/get_brand_template.R
+++ R/get_brand_template.R
@@ -1,3 +1,18 @@
+#' Example Brand Template
+#'
+#' @description
+#' Returns an example [Brand] object..
+#' Includes color palette, Google and system fonts, and typography specs
+#' for base text, headings, and links.
+#'
+#' The template can be inspected as source code via [print_brand_template()],
+#' and used as a starting point for custom branding passed to [create_website()].
+#'
+#' @return A [Brand] object.
+#'
+#' @seealso [print_brand_template()], [Brand()], [create_website()]
+#'
+#' @export
 brand_template <- function() {
   # Begin default branding (leave line for get_brand_template() function)
   Brand(
@@ -34,10 +49,23 @@ brand_template <- function() {
   # End default branding (leave line for get_brand_template() function)
 }
 
-
+#' Print the example Brand Template Source
+#'
+#' @description
+#' Prints the source code of the default brand definition from
+#' [brand_template()] to the console. Useful for inspecting the default
+#' settings or copy-pasting them as a starting point for customization.
+#'
+#'
+#' @return Called for its side effect of printing to the console. Returns
+#'   `NULL` invisibly.
+#'
+#' @seealso [brand_template()]
+#'
+#' @export
 print_brand_template <- function() {
   path_default_brand_function <- system.file(
-    "R/get_brand_template.R",
+    "brand_template.R",
     package = "omni",
     mustWork = TRUE
   )
